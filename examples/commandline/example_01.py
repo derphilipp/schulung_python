b4df89e2570fec415ce6fc005985f3aed783eebe
Philipp <mail@philipp-weissmann.de>
2022-02-01 09:28:58 +0100

    Adds commandline tool click

---
+++ examples/commandline/example_01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A small demonstration of sys.args
+"""
+
+
+def main():
+    print("Hello World!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ examples/commandline/example_02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+A small demonstration of sys.args
+"""
+
+import sys
+
+
+def main():
+    print(sys.argv)
+
+
+if __name__ == "__main__":
+    main()
